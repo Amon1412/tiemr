be44243ec7dc62afdc0debefe0399e60f5198380
Amon1412 <68480041+Amon1412@users.noreply.github.com>
2024-08-09 17:55:51 +0800

    Create RestartServiceWorker.java

---
+++ RestartServiceWorker.java
@@ -0,0 +1,30 @@
+package com.sgtc.countdown;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class RestartServiceWorker extends Worker {
+
+    public RestartServiceWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        // 重新启动 TimerService
+        Context context = getApplicationContext();
+        Intent serviceIntent = new Intent(context, TimerService.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(serviceIntent);
+        } else {
+            context.startService(serviceIntent);
+        }
+        return Result.success();
+    }
+}
